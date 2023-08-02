22449412ba2e6d4685637d790083523b25ce735e
mck0324 <dch3030@naver.com>
2023-08-03 00:17:17 +0900

    ResponseData
    Kotlin + Spring

--- kotlinBoard/src/main/kotlin/com/example/kotlinboard/model/http/UserResponse.kt
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/model/http/UserResponse.kt
@@ -1,10 +1,14 @@
 package com.example.kotlinboard.model.http
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
 
 data class UserResponse (
-
+    var result:Result?=null,
+    var description:String?=null,
+    @JsonProperty(value = "user")
+    var userRequest: List<UserRequest>?=null
 )
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class Result (
