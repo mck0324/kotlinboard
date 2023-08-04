a0a6c6fb4d0a22376d5a7a2b21795733e0a4178b
mck0324 <dch3030@naver.com>
2023-08-05 02:46:35 +0900

    Bean Validation Bean이 아닌 @Validated 변수에 대해서만 진행

--- kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/delete/DeleteApiController.kt
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/delete/DeleteApiController.kt
@@ -2,12 +2,14 @@ package com.example.kotlinboard.controller.delete
 
 import jakarta.validation.constraints.Min
 import jakarta.validation.constraints.NotNull
+import jakarta.validation.constraints.Size
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import kotlin.math.min
 
 @RestController()
 @RequestMapping("/api")
@@ -29,7 +31,10 @@ class DeleteApiController {
     //2. request param
     @DeleteMapping(path = ["/delete-mapping/name/{name}/age/{age}"])
     fun deleteMappingPath(
-            @PathVariable name: String,
+            @PathVariable @Size(min = 2, max = 5, message = "name의 길이는 2~5") @NotNull
+            name: String,
+            @NotNull(message = "age 값이 누락되었습니다.")
+            @Min(value = 20, message = "age는 20보다 커야 합니다.")
             @PathVariable age: Int
     ):String {
         println(name)
