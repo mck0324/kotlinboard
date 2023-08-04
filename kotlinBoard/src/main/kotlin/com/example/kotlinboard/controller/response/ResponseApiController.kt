8a25e693bba9f773442623b212940fd0547f6a5f
mck0324 <dch3030@naver.com>
2023-08-04 16:06:01 +0900

    HttpMethod(POST,PUT,DELETE)로 CRUD ResponseEntity 만들어보기

--- kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/response/ResponseApiController.kt
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/response/ResponseApiController.kt
@@ -1,7 +1,14 @@
 package com.example.kotlinboard.controller.response
 
+import com.example.kotlinboard.model.http.UserRequest
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.PutMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
@@ -43,8 +50,21 @@ class ResponseApiController {
 
     }
     //2.post 200
+    @PostMapping("")
+    fun postMapping(@RequestBody userRequest: UserRequest?): ResponseEntity<Any> {
+        return ResponseEntity.status(200).body(userRequest) //object mapper를 통해 object -> json
+    }
 
     //3.put 201
 
+    @PutMapping("")
+    fun putMapping(@RequestBody userRequest: UserRequest?): ResponseEntity<UserRequest> {
+        //1.기존 데이터가 없어 새로 생성했다 201
+        return ResponseEntity.status(HttpStatus.CREATED).body(userRequest)
+    }
     //4.delete 500
+    @DeleteMapping("/{id}")
+    fun deleteMapping(@PathVariable id: Int): ResponseEntity<Any> {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(null)
+    }
 }
