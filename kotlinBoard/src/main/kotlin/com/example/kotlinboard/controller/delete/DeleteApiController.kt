f239c3f25f9760d17de8b5eb988a4ad1f6671e5f
mck0324 <dch3030@naver.com>
2023-08-05 02:37:57 +0900

    Bean Validation 적용해보기

--- kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/delete/DeleteApiController.kt
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/delete/DeleteApiController.kt
@@ -1,5 +1,8 @@
 package com.example.kotlinboard.controller.delete
 
+import jakarta.validation.constraints.Min
+import jakarta.validation.constraints.NotNull
+import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -8,12 +11,15 @@ import org.springframework.web.bind.annotation.RestController
 
 @RestController()
 @RequestMapping("/api")
+@Validated
 class DeleteApiController {
     //Delete가 갖을 수 있는 Method 형태
     //1. path variable
     @DeleteMapping(path = ["/delete-mapping"])
     fun deleteMapping(
             @RequestParam name: String,
+            @NotNull(message = "age 값이 누락되었습니다.")
+            @Min(value = 20, message = "age는 20보다 커야 합니다.")
             @RequestParam age: Int
     ): String {
         println(name)
