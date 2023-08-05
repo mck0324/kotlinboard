5075b7d7967d1b4191a62b52b892f8a9be8345a1
mck0324 <dch3030@naver.com>
2023-08-05 22:57:42 +0900

    Bean Validation 별도로 로직을 뺴서 에러메세지 보내주기

--- kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/put/PutApiController.kt
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/put/PutApiController.kt
@@ -3,6 +3,10 @@ package com.example.kotlinboard.controller.put
 import com.example.kotlinboard.model.http.Result
 import com.example.kotlinboard.model.http.UserRequest
 import com.example.kotlinboard.model.http.UserResponse
+import jakarta.validation.Valid
+import org.springframework.http.ResponseEntity
+import org.springframework.validation.BindingResult
+import org.springframework.validation.FieldError
 import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -22,38 +26,50 @@ class PutApiController {
         return "request-mapping - put method"
     }
     @PutMapping(path = ["/put-mapping/object"])
-    fun putMappingObject(@RequestBody userRequest: UserRequest): UserResponse {
-        //0.response
-        return UserResponse().apply {
-            //1. result
-            this.result = Result().apply {
-                this.resultCode = "OK"
-                this.resultMessage = "성공"
-            }
-        }.apply {
-            //2. description
-            this.description = "so hot day"
-        }.apply {
-            //3.user
-            val userList = mutableListOf<UserRequest>()
-            userList.add(userRequest)
-            userList.add(UserRequest().apply {
-                this.name = "A"
-                this.age = 10
-                this.email = "dnddnd@dndndn"
-                this.address = "Cal"
-                this.phoneNumber = "11111"
-            })
-            userList.add(UserRequest().apply {
-                this.name = "b"
-                this.age = 20
-                this.email = "hhhhd@dndndn"
-                this.address = "NewYork"
-                this.phoneNumber = "11111"
-            })
-            this.userRequest = userList
+    fun putMappingObject(@Valid @RequestBody userRequest: UserRequest, bindingResult: BindingResult): ResponseEntity<String> {
 
+        if (bindingResult.hasErrors()) {
+            //500error
+            val msg = StringBuilder()
+            bindingResult.allErrors.forEach {
+                val field = it as FieldError
+                val message = it.defaultMessage
+                msg.append(field.field+" : "+message+"\n")
+            }
+            return ResponseEntity.badRequest().body(msg.toString())
         }
+        return ResponseEntity.ok("ok")
+        //0.response
+//        return UserResponse().apply {
+//            //1. result
+//            this.result = Result().apply {
+//                this.resultCode = "OK"
+//                this.resultMessage = "성공"
+//            }
+//        }.apply {
+//            //2. description
+//            this.description = "so hot day"
+//        }.apply {
+//            //3.user
+//            val userList = mutableListOf<UserRequest>()
+//            userList.add(userRequest)
+//            userList.add(UserRequest().apply {
+//                this.name = "A"
+//                this.age = 10
+//                this.email = "dnddnd@dndndn"
+//                this.address = "Cal"
+//                this.phoneNumber = "11111"
+//            })
+//            userList.add(UserRequest().apply {
+//                this.name = "b"
+//                this.age = 20
+//                this.email = "hhhhd@dndndn"
+//                this.address = "NewYork"
+//                this.phoneNumber = "11111"
+//            })
+//            this.userRequest = userList
+//
+//        }
 
     }
 }
--- kotlinBoard/src/main/kotlin/com/example/kotlinboard/model/http/UserRequest.kt
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/model/http/UserRequest.kt
@@ -3,15 +3,32 @@ package com.example.kotlinboard.model.http
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.databind.PropertyNamingStrategy
 import com.fasterxml.jackson.databind.annotation.JsonNaming
+import jakarta.validation.constraints.Email
+import jakarta.validation.constraints.NotBlank
+import jakarta.validation.constraints.NotEmpty
+import jakarta.validation.constraints.Pattern
+import jakarta.validation.constraints.PositiveOrZero
+import jakarta.validation.constraints.Size
+import kotlin.math.min
 
 //@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)
 data class UserRequest (
+
+        @field:NotEmpty
+        @field:Size(min = 2, max = 8)
         var name:String?= null,
+
+        @field:PositiveOrZero // 0 보다 큰 숫자 검사 (0포함)
         var age:Int ?= null,
+
+        @field:Email //이메일 양식
         var email:String?= null,
+
+        @field:NotBlank // 공백 검증
         var address:String?= null,
 //        var phoneNumber:String?= null, //restapi는 보통 스네이크식으로 phone_number로 함 이렇게하면 안되므로
 //        @JsonProperty("phone_number")
+        @field:Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}\$") //정규식 검증
         var phoneNumber:String?= null
         //이렇게해도 되지만 회사마다 정책이 다다르고 매번 이렇게 해줄 수 없기에
         //@JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy::class)를 이용
