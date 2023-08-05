249df4d6e13c1e017ed59bc9bdcd878aee6414c5
mck0324 <dch3030@naver.com>
2023-08-06 01:47:46 +0900

    예외처리 (전체적인 일)

---
+++ kotlinBoard/src/main/kotlin/com/example/kotlinboard/controller/exception/ExceptionApiController.kt
@@ -0,0 +1,16 @@
+package com.example.kotlinboard.controller.exception
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+@RequestMapping("/api/exception")
+class ExceptionApiController {
+
+    @GetMapping("/hello")
+    fun hello() {
+        val list = mutableListOf<String>()
+        val temp = list[0]
+    }
+}
